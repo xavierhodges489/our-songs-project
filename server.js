1dc2137a2eb4c2090a3df61da91f0672223c18ef
Xavier Hodges <xavierhodges489@gmail.com>
2019-11-16 19:51:05 -0500

    used express.static in server.js

--- server.js
+++ server.js
@@ -15,9 +15,13 @@ app.use("/api/posts", postsRouter);
 app.use("/api/token", spotifyTokenRouter);
 app.use("/api/users", usersRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
