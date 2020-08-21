c5448e567a1b5b9b21a9dfb0f1715a8dc09c7fc3
Xavier Hodges <xavierhodges489@gmail.com>
2020-08-21 00:01:50 -0400

    updated database name

--- routes/db.js
+++ routes/db.js
@@ -5,15 +5,15 @@ const config = {
   password: process.env.DATABASE_PASSWORD,
   server: process.env.DATABASE_URI,
   port: 1433,
-  database: "DistWeb"
+  database: "OurSongsDB",
 };
 
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
-  .then(pool => {
+  .then((pool) => {
     console.log("connected to MSSQL database");
     return pool;
   })
-  .catch(err => console.log("database connection failed"));
+  .catch((err) => console.log("database connection failed"));
 
 module.exports = { sql, poolPromise };
